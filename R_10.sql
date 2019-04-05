413c43332beae9d175ad3f2b04242bc5c1fe614f
Hugo <32120709+nilitz@users.noreply.github.com>
2019-04-05 10:14:43 +0200

    Update R_10.sql

--- R_10.sql
+++ R_10.sql
@@ -1,25 +1,24 @@
-CREATE TEMPORARY TABLE t1
-SELECT rapporte.ID, SUM(rapporte.Quantite*oberon.CoefficientDeConversion) AS ObelosDeBronze
-FROM rapporte JOIN oberon ON (rapporte.ID_Oberon = oberon.ID)
-GROUP BY rapporte.ID;
-
-UPDATE t1, vente
-SET t1.ObelosDeBronze = (t1.ObelosDeBronze / vente.quantite) WHERE vente.ID = t1.ID;
-
-CREATE TEMPORARY TABLE t2
-SELECT vente.ID, t1.ObelosDeBronze, vente.ID_Divinite, fabrique.ID_Artisan
-FROM t1
-INNER JOIN vente ON t1.ID = vente.ID
-INNER JOIN fabrique ON fabrique.ID = t1.ID
-WHERE vente.ID_Divinite IS NOT NULL AND fabrique.ID_Artisan IS NOT NULL;
-
-CREATE TEMPORARY TABLE t3
-SELECT ID_Artisan, ID_divinite, MAX(t2.ObelosDeBronze) AS Prixmaxi FROM t2 GROUP BY t2.ID_Artisan;
-
-_____________________________________________________________________________________________________
-
-SELECT rapporte.ID, ROUND(SUM((rapporte.Quantite * oberon.CoefficientDeConversion)/vente.Quantite))
+CREATE TEMPORARY TABLE T1
+SELECT rapporte.ID, SUM((rapporte.Quantite * oberon.CoefficientDeConversion)/vente.Quantite) AS O_Bronze
 FROM rapporte
 LEFT JOIN oberon ON rapporte.ID_Oberon = oberon.ID
 LEFT JOIN vente ON rapporte.ID = vente.ID
-GROUP BY rapporte.ID
+GROUP BY rapporte.ID;
+
+CREATE TEMPORARY TABLE T2
+SELECT vente.ID, T1.O_Bronze, vente.ID_Divinite, fabrique.ID_Artisan
+FROM T1
+INNER JOIN vente ON T1.ID = vente.ID
+INNER JOIN fabrique ON fabrique.ID = T1.ID
+WHERE vente.ID_Divinite IS NOT NULL AND fabrique.ID_Artisan IS NOT NULL;
+
+CREATE TEMPORARY TABLE T3
+SELECT ID_Artisan, artisan.Nom AS Artisans, ID_divinite, divinite.Nom AS Divinités, ROUND(AVG(T2.O_Bronze)) AS `Prix moyen des ventes de l'artisan`
+FROM T2
+INNER JOIN artisan ON T2.ID_artisan = artisan.ID
+INNER JOIN divinite ON T2.ID_divinite = divinite.ID
+GROUP BY T2.ID_Artisan;
+
+SELECT Artisans AS `Nom de l'artisan`, Divinités AS `Divinité la plus rentable pour l'artisan`, `Prix moyen des ventes de l'artisan` FROM T3 ORDER BY `Prix moyen des ventes de l'artisan` DESC;
+
+DROP TABLE T1, T2, T3; 
