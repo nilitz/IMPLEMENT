c23fa6601be872829c80050b3a53c441fde821c6
Hugo <32120709+nilitz@users.noreply.github.com>
2019-04-05 13:31:45 +0200

    Update R_9.sql

--- R_9.sql
+++ R_9.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 CREATE PROCEDURE requete_neuf (IN artisan_id INT)
 BEGIN
-		SELECT SUBSTRING_INDEX(vente.DATE, ",", 1) AS date_actuelle, fabrique.ID_Artisan AS `artisan`, SUM(vente.Quantite) AS quantite
+		SELECT SUBSTRING_INDEX(vente.DATE, ",", 1) AS `Date (Année)`, fabrique.ID_Artisan AS `ID de l'artisan`, SUM(vente.Quantite) AS `Quantité vendue par l'artisan`
 		FROM vente
 		LEFT JOIN fabrique ON vente.ID = fabrique.ID
 		WHERE fabrique.ID_Artisan = artisan_id
