b37becae80560b93a9003d0a6cbb07d5cf9d2c54
Hugo <32120709+nilitz@users.noreply.github.com>
2019-04-05 09:58:37 +0200

    Update R_9.sql

--- R_9.sql
+++ R_9.sql
@@ -1,10 +1,9 @@
-SELECT T0.date_actuelle, T0.`artisan`, T0.quantite
-FROM 
-(
-	SELECT (SUBSTRING_INDEX(vente.DATE, ",", 1)-1) AS date_posterieur, SUBSTRING_INDEX(vente.DATE, ",", 1) AS date_actuelle, fabrique.ID_Artisan AS `artisan`, SUM(vente.Quantite) AS quantite
-	FROM vente
-	LEFT JOIN fabrique ON vente.ID = fabrique.ID
-	WHERE fabrique.ID_Artisan = '1'
-	GROUP BY fabrique.ID_Artisan, SUBSTRING_INDEX(vente.DATE, ",", 1)
-	
-) AS T0
+DELIMITER $$
+CREATE PROCEDURE requete_neuf (IN artisan_id INT)
+BEGIN
+		SELECT SUBSTRING_INDEX(vente.DATE, ",", 1) AS date_actuelle, fabrique.ID_Artisan AS `artisan`, SUM(vente.Quantite) AS quantite
+		FROM vente
+		LEFT JOIN fabrique ON vente.ID = fabrique.ID
+		WHERE fabrique.ID_Artisan = artisan_id
+		GROUP BY fabrique.ID_Artisan, SUBSTRING_INDEX(vente.DATE, ",", 1);
+END $$
